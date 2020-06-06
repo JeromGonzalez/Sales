ec2fe293279404ada43eb67cf74d6a6374cc12ae
JeromGonzalez <jerom_g@hotmail.com>
2020-06-06 12:07:29 +0200

    Controlador Articulos

---
+++ Sales.Backend/Controllers/ArticulosController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sales.Backend.Models;
+using Sales.Common.Models;
+
+namespace Sales.Backend.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        private LocalDataContext db = new LocalDataContext();
+
+        // GET: Articulos
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Articulos.ToListAsync());
+        }
+
+        // GET: Articulos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulos articulos = await db.Articulos.FindAsync(id);
+            if (articulos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulos);
+        }
+
+        // GET: Articulos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "CodProducto,Descripcion,PVP,Activo,FechaAlata")] Articulos articulos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articulos.Add(articulos);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(articulos);
+        }
+
+        // GET: Articulos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulos articulos = await db.Articulos.FindAsync(id);
+            if (articulos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulos);
+        }
+
+        // POST: Articulos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "CodProducto,Descripcion,PVP,Activo,FechaAlata")] Articulos articulos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulos).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(articulos);
+        }
+
+        // GET: Articulos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulos articulos = await db.Articulos.FindAsync(id);
+            if (articulos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulos);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Articulos articulos = await db.Articulos.FindAsync(id);
+            db.Articulos.Remove(articulos);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
